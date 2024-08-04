d05469fb24005c5b1eae7dd070d6c228eeff427e
JuniorSierra <juniorsierramorales2002@gmailcom>
2024-08-04 18:26:08 -0500

    Diseño de la interfaz inicial

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -10,13 +14,47 @@ func main() {
 	//Crea la app
 	a := app.New()
 	//Crea la ventana y le pone el nombre de esta
-	w := a.NewWindow("Hello World")
-	//Tamaño de la ventana ancho y alto
-	w.Resize(fyne.NewSize(600, 500))
-	//Texto de la ventana
-	label := widget.NewLabel("Hello World")
-	//Muestra el texto en la ventan
-	w.SetContent(label)
+	w := a.NewWindow("Memory Game")
+
+	// Etiqueta para el titulo
+	titulo := canvas.NewText("Memory Game", color.White)
+	titulo.Alignment = fyne.TextAlignCenter
+	titulo.TextSize = 24
+
+
+	//Botones de Inicio
+	botonJugar := widget.NewButton("Jugar", func ()  {
+		//Pasar a la siguiente ventana
+	})
+
+	botonIntrucciones := widget.NewButton("Intrucciones", func ()  {
+		//Mostrar las intrucciones
+	})
+
+	botonSalir := widget.NewButton("Salir", func() {
+		//Cierra la aplicacion
+		a.Quit()
+	})
+
+	// Estilo de los botones
+	botonJugar.Importance = widget.HighImportance
+	botonIntrucciones.Importance = widget.HighImportance
+	botonSalir.Importance = widget.DangerImportance
+
+	// Contenedor
+	content := container.NewVBox(
+		titulo,
+		container.NewVBox(botonJugar, botonIntrucciones),
+		botonSalir,
+	)
+
+	// Centra el contenido
+	contentCenter := container.NewCenter(content)
+	// Agrega el contenido a la ventana
+	w.SetContent(contentCenter)
+	// Define el tamaño de la ventana
+	w.Resize(fyne.NewSize(500, 500))
+
 	//Corre la aplicaion
 	w.ShowAndRun()
 }
